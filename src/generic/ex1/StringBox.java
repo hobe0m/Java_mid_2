6ab5caa8f56efa969703d442e7297c53d7c455ad
hobe0m <beom231231@gmail.com>
2024-09-12 23:01:21 +0900

    [finish]Generic

---
+++ src/generic/ex1/StringBox.java
@@ -0,0 +1,15 @@
+package generic.ex1;
+
+public class StringBox {
+    private String value;
+
+    public void set(String object) {
+        this.value = value;
+    }
+
+    public String get() {
+        return value;
+    }
+
+    // 문자열을 보관하고 꺼낼 수 있는 단순한 기능을 제공한다.
+}
