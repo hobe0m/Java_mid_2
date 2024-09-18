40c5d30eb8233079b78d4e62d8a187c40f5fccdc
hobe0m <beom231231@gmail.com>
2024-09-18 20:19:55 +0900

    [finish]Generic

--- src/generic/ex1/GenericBox.java
+++ src/generic/ex1/GenericBox.java
@@ -1,6 +1,10 @@
 package generic.ex1;
 
 public class GenericBox<T> {
+    // 제네릭 적용
+    // 제네릭 적용 시 코드 재사용과 타입 안전성이라는 두 마리 토끼를 한번에 잡을 수 있다.
+    // 이름 옆에 <T> 다이아몬드 사이에 있는 T를 적어준다.
+    //  - 이 때, T말고 다른 것도 사용 가능, T는 타입을 넣어주는 타입 매개 변수
 
     private T value;
 
