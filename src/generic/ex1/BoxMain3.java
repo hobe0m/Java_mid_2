83f52cb48eeaa306834316822e95978d2c6ac27e
hobe0m <beom231231@gmail.com>
2024-09-18 20:26:05 +0900

    [finish]Generic

--- src/generic/ex1/BoxMain3.java
+++ src/generic/ex1/BoxMain3.java
@@ -3,26 +3,28 @@ package generic.ex1;
 public class BoxMain3 {
 
     public static void main(String[] args) {
-        GenericBox<Integer> integerBox = new GenericBox<Integer>(); // 생성 시점에서 T의 타입 결정
+        // 제네릭 클래스 사용
+
+        GenericBox<Integer> integerBox = new GenericBox<Integer>(); // 생성 시점에서 T의 타입 결정, 여기서는 Integer
         integerBox.set(10);
-        // integerBox.set("문자100"); Integer 타입만 허용하므로, 컴파일 오류 발생
-        Integer integer = integerBox.get(); // Integer 타입 변환 (캐스팅 X)
+        // integerBox.set("문자100"); Integer 타입만 허용하므로, 컴파일 오류 발생 : Generic을 사용해서 얻을 수 있는 효과
+        Integer integer = integerBox.get(); // Integer 타입 변환 (캐스팅 X) : Generic을 사용해서 얻을 수 있는 효과
         System.out.println("integer = " + integer);
 
-        GenericBox<String> stringBox = new GenericBox<String>(); // 생성 시점에서 T의 타입 결정
+        GenericBox<String> stringBox = new GenericBox<String>(); // 생성 시점에서 T의 타입 결정, 여기서는 String
         stringBox.set("hello"); // String 타입만 허용
         String str = stringBox.get(); // String 타입만 반환
         System.out.println("str = " + str);
 
-        GenericBox<Double> doubleBox = new GenericBox<Double>(); // 생성 시점에서 T의 타입 결정
-        doubleBox.set(10.5);
-        Double doubleValue = doubleBox.get();
+        GenericBox<Double> doubleBox = new GenericBox<Double>(); // 생성 시점에서 T의 타입 결정, 여기서는 Double
+        doubleBox.set(10.5); // Double 타입만 허용
+        Double doubleValue = doubleBox.get(); // Double 타입만 반환
         System.out.println("doubleValue = " + doubleValue);
 
         // 타입 추론 : 생성하는 제네릭 타입 생략 가능
         // new 다음 제네릭 박스에는 타입을 써주지 않아도 된다.
         GenericBox<Integer> integerBox2 = new GenericBox<>();
-
+        // 앞에서 이미 Integer라고 선언했기 때문에, 당연히 Integer가 들어갈 것이라고 추론한다.
     }
 }
 
