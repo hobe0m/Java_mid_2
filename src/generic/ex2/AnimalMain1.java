237cad201c4dd06d629c26db86530ad9eac30683
hobe0m <beom231231@gmail.com>
2024-09-18 21:16:32 +0900

    [finish]Generic

--- src/generic/ex2/AnimalMain1.java
+++ src/generic/ex2/AnimalMain1.java
@@ -11,13 +11,23 @@ public class AnimalMain1 {
         Cat cat = new Cat("고양이", 50);
 
         Box<Dog> dogBox = new Box<>();
-        dogBox.set(dog); // 다른 것을 넣으면 컴파일 오류 발생
+        dogBox.set(dog); // 강아지 말고 다른 것을 넣으면 컴파일 오류 발생
         Dog findDog = dogBox.get();
         System.out.println("findDog = " + findDog);
 
         Box<Cat> catBox = new Box<>();
         catBox.set(cat);
-        Cat findCat = catBox.get();
+        Cat findCat = catBox.get(); // 마찬가지로 고양이 말고 다른 것을 넣으면 컴파일 오류 발생
         System.out.println("findCat = " + findCat);
+
+        Box<Animal> animalBox = new Box<>();
+        animalBox.set(animal);
+        Animal findAnimal = animalBox.get();
+        System.out.println("findAnimal = " + findAnimal);
+
+        // Box 제네릭 클래스에 각각의 타입에 맞는 동물들을 보관하고 꺼낸다.
+        // Box<Dog> dogBox : Dog 타입을 보관할 수 있다.
+        // Box<Cat> catBox : Cat 타입을 보관할 수 있다.
+        // Box<Animal> animalBox : Animal 타입을 보관할 수 있다.
     }
 }
---
+++ src/generic/ex2/AnimalMain2.java
@@ -0,0 +1,27 @@
+package generic.ex2;
+
+import generic.animal.Animal;
+import generic.animal.Cat;
+import generic.animal.Dog;
+
+public class AnimalMain2 {
+    public static void main(String[] args) {
+        Animal animal = new Animal("동물", 0);
+        Dog dog = new Dog("강아지", 100);
+        Cat cat = new Cat("고양이", 50);
+
+        Box<Animal> animalBox = new Box<>();
+
+        // Animal이 부모이므로, 하위 타입인 Dog와 Cat도 들어갈 수 있다.
+
+        animalBox.set(animal);
+        animalBox.set(dog);
+        animalBox.set(cat);
+
+        Animal findAnimal = animalBox.get();
+        System.out.println("findAnimal = " + findAnimal);
+
+        // 또한 set(Animal value)이므로 set()에 Animal의 하위 타입인 Dog, Cat도 전달할 수 있다.
+        // 물론 이 경우 꺼낼 때는 Animal 타입으로만 꺼낼 수 있다.
+    }
+}
