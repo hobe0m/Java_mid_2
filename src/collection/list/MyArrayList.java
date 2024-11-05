30d3b849f2de8628f3814b78d043fcf96236d153
hobe0m <beom231231@gmail.com>
2024-11-06 07:42:02 +0900

    [finish]Collection

---
+++ src/collection/list/MyArrayList.java
@@ -0,0 +1,39 @@
+package collection.list;
+
+public class MyArrayList<E> implements MyList {
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public void add(Object object) {
+
+    }
+
+    @Override
+    public void add(int index, Object object) {
+
+    }
+
+    @Override
+    public Object get(int index) {
+        return null;
+    }
+
+    @Override
+    public Object set(int index, Object element) {
+        return null;
+    }
+
+    @Override
+    public Object remove(int index) {
+        return null;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return 0;
+    }
+}
