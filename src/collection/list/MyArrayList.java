ba418ed73ad18bfe7b53ce0cc432c8fc117cd7f3
hobe0m <beom231231@gmail.com>
2024-11-08 18:29:58 +0900

    [finish]Collection

--- src/collection/list/MyArrayList.java
+++ src/collection/list/MyArrayList.java
@@ -1,39 +1,85 @@
 package collection.list;
 
-public class MyArrayList<E> implements MyList {
+import java.util.Arrays;
 
+public class MyArrayList<E> implements MyList<E> {
+
+    private static final int DEFAULT_CAPACITY = 5;
+    private Object[] elementData;
+    private int size = 0;
+    public MyArrayList() {
+        elementData = new Object[DEFAULT_CAPACITY];
+    } public MyArrayList(int initialCapacity) {
+        elementData = new Object[initialCapacity];
+    }
     @Override
     public int size() {
-        return 0;
+        return size;
     }
-
     @Override
-    public void add(Object object) {
-
+    public void add(E e) {
+        if (size == elementData.length) {
+            grow();
+        }
+        elementData[size] = e;
+        size++;
     }
-
     @Override
-    public void add(int index, Object object) {
-
+    public void add(int index, E e) {
+        if (size == elementData.length) {
+            grow();
+        }
+        shiftRightFrom(index);
+        elementData[index] = e;
+        size++;
     }
-
-    @Override
-    public Object get(int index) {
-        return null;
+    //요소의 마지막부터 index까지 오른쪽으로 밀기
+    private void shiftRightFrom(int index) {
+        for (int i = size; i > index; i--) {
+            elementData[i] = elementData[i - 1];
+        }
     }
-
     @Override
-    public Object set(int index, Object element) {
-        return null;
+    @SuppressWarnings("unchecked")
+    public E get(int index) {
+        return (E) elementData[index];
     }
-
     @Override
-    public Object remove(int index) {
-        return null;
+    public E set(int index, E element) { E oldValue = get(index);
+        elementData[index] = element;
+        return oldValue;
     }
-
     @Override
-    public int indexOf(Object o) {
-        return 0;
+    public E remove(int index) {
+        E oldValue = get(index);
+        shiftLeftFrom(index);
+        size--;
+        elementData[size] = null;
+        return oldValue;
+    }
+    //요소의 index부터 마지막까지 왼쪽으로 밀기
+    private void shiftLeftFrom(int index) {
+        for (int i = index; i < size - 1; i++) {
+            elementData[i] = elementData[i + 1];
+        }
+    }
+    @Override
+    public int indexOf(E o) {
+        for (int i = 0; i < size; i++) {
+            if (o.equals(elementData[i])) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    private void grow() {
+        int oldCapacity = elementData.length;
+        int newCapacity = oldCapacity * 2;
+        elementData = Arrays.copyOf(elementData, newCapacity);
+    }
+    @Override
+    public String toString() {
+        return Arrays.toString(Arrays.copyOf(elementData, size)) + " size=" +
+                size + ", capacity=" + elementData.length;
     }
 }
