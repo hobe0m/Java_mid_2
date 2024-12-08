2b99ceb0bd1e5c37a9e0e35247c3978e7b96a8cd
hobe0m <beom231231@gmail.com>
2024-12-08 10:33:31 +0900

    [finish]Collection

--- src/collection/set/member/HashAndEqualMain2.java
+++ src/collection/set/member/HashAndEqualMain2.java
@@ -1,21 +1,20 @@
-package collection.set;
+package collection.set.member;
 
-import collection.set.member.MemberNoHashNoEq;
+import collection.set.MyHashSetV2;
 
-public class HashAndEqualsMain1 {
+public class HashAndEqualMain2 {
     public static void main(String[] args) {
         //중복 등록
         MyHashSetV2 set = new MyHashSetV2(10);
-        MemberNoHashNoEq m1 = new MemberNoHashNoEq("A");
-        MemberNoHashNoEq m2 = new MemberNoHashNoEq("A");
+        MemberOnlyHash m1 = new MemberOnlyHash("A");
+        MemberOnlyHash m2 = new MemberOnlyHash("A");
         System.out.println("m1.hashCode() = " + m1.hashCode());
         System.out.println("m2.hashCode() = " + m2.hashCode());
         System.out.println("m1.equals(m2) = " + m1.equals(m2));
-        set.add(m1);
-        set.add(m2);
+        set.add(m1); set.add(m2);
         System.out.println(set);
         //검색 실패
-        MemberNoHashNoEq searchValue = new MemberNoHashNoEq("A");
+        MemberOnlyHash searchValue = new MemberOnlyHash("A");
         System.out.println("searchValue.hashCode() = " +
                 searchValue.hashCode());
         boolean contains = set.contains(searchValue);
