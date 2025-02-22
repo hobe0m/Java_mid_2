c972702f4829aaa26b4230c3055b4098027c36b1
hobe0m <beom231231@gmail.com>
2025-02-22 19:23:23 +0900

    [finish]Generic

--- src/generic/animal/Dog.java
+++ src/generic/animal/Dog.java
@@ -5,6 +5,8 @@ import org.w3c.dom.ls.LSOutput;
 public class Dog extends Animal {
     public Dog(String name, int size) {
         super(name, size);
+        // 슈퍼 클래스에 있는 생성자를 만들어준다.
+        // 부모를 상속받아, 부모의 필드도 사용할 수 있기 때문에 반드시 포함해야 한다.
     }
 
     // ctrl + o : 오버라이딩
