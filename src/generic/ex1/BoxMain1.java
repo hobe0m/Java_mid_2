f6d5627708003134222704ede551412fd9da1649
hobe0m <beom231231@gmail.com>
2025-02-22 18:33:58 +0900

    [finish]Generic

--- src/generic/ex1/BoxMain1.java
+++ src/generic/ex1/BoxMain1.java
@@ -4,7 +4,7 @@ public class BoxMain1 {
 
     public static void main(String[] args) {
         IntegerBox integerBox = new IntegerBox();
-        integerBox.set(10); // 오토 박싱
+        integerBox.set(10); // 오토 박싱, int가 integer로 변환(자동)
         Integer integer = integerBox.get();
         System.out.println("integer = " + integer);
 
@@ -21,4 +21,5 @@ public class BoxMain1 {
     // 이후에 더 다양한 타입을 담는 박스가 필요하면 박스 클래스를 계속 만들어야 하는 불편함이 있다.
     // 코드는 같은데 타입만 다른 박스들이 필요해지니까 제네릭을 사용한다.
     // 따라서 제네릭이 등장했다.
+    //  - 제네릭은 타입을 지정하지 않기 때문에, 상황에 맞는 타입을 받을 수 있다.
 }
