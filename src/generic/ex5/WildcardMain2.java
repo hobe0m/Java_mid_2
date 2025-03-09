7bbc27d0c6654dab369a75256d3cba466966d318
hobe0m <beom231231@gmail.com>
2025-03-09 18:59:46 +0900

    [finish]Generic

--- src/generic/ex5/WildcardMain2.java
+++ src/generic/ex5/WildcardMain2.java
@@ -16,13 +16,15 @@ public class WildcardMain2 {
         writeBox(objBox);
         writeBox(animalBox);
 
-        // writeBox(dogBox); // 하한이 Animal
-        // writeBox(catBox); // 하한이 Animal
+        // writeBox(dogBox); // 하한이 Animal이어야 한다.
+        // writeBox(catBox); // 하한이 Animal이어야 한다.
         Animal animal = animalBox.get();
         System.out.println("animal = " + animal);
     }
 
     // 메서드에서 super를 사용하므로 animal 자신이거나 animal 보다 높아야 한다.
+    // ?, 와일드 카드는 Animal 보다 상위에 위치해야 한다.
+    // ?는 Animal의 상위 타입이라는 뜻
     static void writeBox(Box<? super Animal> box) {
         box.set(new Dog("멍멍이", 100));
     }
