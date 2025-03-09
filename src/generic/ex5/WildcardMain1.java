1a70e27aa92a533123900b54e2b9a41581366926
hobe0m <beom231231@gmail.com>
2025-03-09 17:09:19 +0900

    [finish]Generic

--- src/generic/ex5/WildcardMain1.java
+++ src/generic/ex5/WildcardMain1.java
@@ -19,6 +19,8 @@ public class WildcardMain1 {
         WildcardEx.printWildcardV2(dogBox);
 
         Dog dog = WildcardEx.printAndReturnGeneric(dogBox);
+
+        // 반환 타입이 Animal이기 때문에 Animal로 받아야 한다.
         Animal animal = WildcardEx.printAndReturnWildcard(dogBox);
     }
 }
